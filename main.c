f0e6bb95fbb3c944eecd3197f5a5bacd8731f7b1
Thomas Schwartz <Thomas.Schwartz@cpnv.ch>
2021-03-06 18:38:57 +0100

    Ajout des question pour la position et de la vérification

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <windows.h>
 
+
+const int Bateaux [10][10] = {{1,1,1,0,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,0,1,1,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0},
+                              {0,1,0,0,0,0,0,0,0,0},
+                              {0,1,0,0,0,0,0,0,0,0},
+                              {0,0,0,0,0,0,0,0,0,0}};
+
+
 /*
 *
 */
@@ -30,26 +43,41 @@ int choixDuMenu()
 }
 
 void Regle(){
-        printf("\nLes regles de la bataille navale :\n");
-        printf("\nVous aurez une grille de 10 sur 10 et vous devrez choisir une case");
-        printf("\nUne fois une case choisit, le programme lancera un boulet dessus et vous redira si il y a vait un beateau ou non");
-        printf("\n\nPoint important :\n");
-        printf("\nles casse s'afficheron comme ceci : ");
-        printf("\n\nUne fois une case touchée elle pourra s'afficher de 2 façon :\n\n");
-        printf("Si il y avait quelque chose sur la case :  \n");
-        printf("Si il n'y avait rien sur la case : █ ");
-        printf("\n\nLe jeux se terminera une fois tout les bateau coullé");
+    printf("\nLes regles de la bataille navale :\n");
+    printf("\nVous aurez une grille de 10 sur 10 et vous devrez choisir une case");
+    printf("\nUne fois une case choisit, le programme lancera un boulet dessus et vous redira si il y a vait un beateau ou non");
+    printf("\n\nPoint important :\n");
+    printf("\nles casse s'afficheron comme ceci : A");
+    printf("\n\nUne fois une case touchée elle pourra s'afficher de 2 façon :\n\n");
+    printf("Si il y avait quelque chose sur la case : B\n");
+    printf("Si il n'y avait rien sur la case : C");
+    printf("\n\nLe jeux se terminera une fois tout les bateau coullé");
 
 };
 
-void demandeUneCase(){
-    printf("choisisez une case (Exemple : F4");
-    printf("Vetical : ");
 
-
-
-
+int demandeUneCaseVertical(){
+    int chiffreV;
+    printf("\nchoisisez une case (Exemple : 1-4 (1 = vertical))");
+    printf("\nVertical: ");
+    scanf("%s",&chiffreV);
+    return chiffreV;
 };
+int demandeUneCaseHorizontal(){
+    int chiffreH;
+    printf("\nchoisisez une case (Exemple : 1-4 (4 = horizontal))");
+    printf("\nHorizontal : ");
+    scanf("%s",&chiffreH);
+    return chiffreH;
+}
+
+void verifieSiToucher(int x,int y){
+    if (Bateaux[y-49][x-49]==1){
+        printf("\ntoucher");
+    }else{
+        printf("y a rien");
+    }
+}
 
 
 
@@ -58,25 +86,29 @@ int main() {
     SetConsoleOutputCP(65001);
 
 
-        menu();
+    menu();
 
 
 
-        switch (choixDuMenu()) {
-            case 0:
-                return 0;
+    switch (choixDuMenu()) {
+        case 0:
+            return 0;
 
-            case 1:
-                demandeUneCase;
-                //y aura le jeux
-                break;
+        case 1:
 
-            case 2 :
-                Regle();
 
-                system("pause");
-                break;
+            verifieSiToucher(demandeUneCaseHorizontal(),demandeUneCaseVertical());
+            system("pause");
 
-        }
+            //y aura le jeux
+            break;
+
+        case 2 :
+            Regle();
+
+            system("pause");
+            break;
+
+    }
 
 }
